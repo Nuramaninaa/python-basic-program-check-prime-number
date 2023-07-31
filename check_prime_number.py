995b64207036a4be13dbe4f264a535cfe5351015
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-31 14:32:54 +0800

    Add files via upload

---
+++ check_prime_number.py
@@ -0,0 +1,13 @@
+#python program to check prime number
+
+
+number = int(input("Enter the number : "))
+
+if number > 1 :
+    for i in range(2, number):
+        if  (number % i) ==0:
+             print ("is not a prime number", number)
+             break
+
+    else:
+        print ("is a prime number", number)
